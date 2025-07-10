126ada47d3d5b106019d23c45832edd52fc99d79
Lukas Tomek <l.tomek@kostal.com>
2025-07-10 17:19:35 +0200

    Next try

--- components/cm1106_sniffer/sensor.py
+++ components/cm1106_sniffer/sensor.py
@@ -12,15 +12,17 @@ DEPENDENCIES = ["uart"]
 cm1106_ns = cg.esphome_ns.namespace('cm1106_sniffer')
 CM1106Sniffer = cm1106_ns.class_('CM1106SnifferSensor', sensor.Sensor, cg.PollingComponent, uart.UARTDevice)
 
-CONFIG_SCHEMA = (sensor.sensor_schema(
-    unit_of_measurement=UNIT_PARTS_PER_MILLION,
-    icon=ICON_MOLECULE_CO2,
-    accuracy_decimals=0,
-    device_class=DEVICE_CLASS_CARBON_DIOXIDE,
-    state_class='measurement',
-).extend(cv.polling_component_schema("5s"))
-.extend(uart.UART_DEVICE_SCHEMA)
-.extend({cv.GenerateID(): cv.declare_id(CM1106Sniffer),})
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        unit_of_measurement=UNIT_PARTS_PER_MILLION,
+        icon=ICON_MOLECULE_CO2,
+        accuracy_decimals=0,
+        device_class=DEVICE_CLASS_CARBON_DIOXIDE,
+        state_class='measurement',
+    )
+    .extend(cv.polling_component_schema("5s"))
+    .extend(uart.UART_DEVICE_SCHEMA)
+    .extend({cv.GenerateID(): cv.declare_id(CM1106Sniffer),})
 )
 
 FINAL_VALIDATE_SCHEMA = uart.final_validate_device_schema(
